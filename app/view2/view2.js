7df6adf36a533ce7c057ec40ed258c2ea6ac8d38
Jitendra <jitendra@beyondroot.com>
2018-07-05 11:13:02 +0530

    initial commit

---
+++ app/view2/view2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl as vm'
+  });
+}])
+
+.controller('View2Ctrl', [function($scope) {
+
+  var vm = this;
+  
+  vm.Name = "jitendra"
+
+}]);
